3ebac09fa90747ccbe229998d5909d3e6db08f05
Dave Stewart <dave.c.stewart@gmail.com>
2022-02-13 08:35:20 -0500

    Finished grid

--- grid.js
+++ grid.js
@@ -1,3 +1,6 @@
+// This file is used to add the grid to the canvas to facilitate development.
+// Feel free to play around with this file to change the look of the grid. If
+// needed, a fresh copy and be downloaded from GitHub.
 function addGrid(Konva, layer) {
 
     for (i = 50; i <= layer.width(); i += 50) {
@@ -5,8 +8,17 @@ function addGrid(Konva, layer) {
             points: [i, 0, i, layer.height()],
             stroke: '#CCC',
             strokeWidth: 1,
-          });
-          layer.add(line)
+        });
+        layer.add(line)
+        let text = new Konva.Text({
+            x: i - 8,
+            y: 0,
+            text: i,
+            fontSize: 10,
+            fontFamily: 'Calibri',
+            fill: '#999',
+        });
+        layer.add(text)
     }
 
     for (i = 50; i <= layer.height(); i += 50) {
@@ -14,8 +26,16 @@ function addGrid(Konva, layer) {
             points: [0, i, layer.width(), i],
             stroke: '#CCC',
             strokeWidth: 1,
-          });
-          layer.add(line)
+        });
+        layer.add(line)
+        let text = new Konva.Text({
+            x: 0,
+            y: i - 10,
+            text: i,
+            fontSize: 10,
+            fontFamily: 'Calibri',
+            fill: '#999',
+        });
+        layer.add(text)
     }
-
 }
